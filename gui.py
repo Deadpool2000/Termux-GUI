7da3fca49e8e67068e73862735453191d37ddef2
Deadpool2000 <thetrickboy007@gmail.com>
2020-02-11 09:31:55 +0530

    gui.py

--- gui.py
+++ gui.py
@@ -10,10 +10,10 @@ try:
     B='\033[95m' #Blue
 
     print(Y+"""
-<----------------------------------------->"""+G+"""
-           Termux GUI Installer"""+Y+"""
-<----------------------------------------->"""+R+"""
-        == """+CY+"""By Deadpool2000 """+R+"""==
+<------------------------------------------------------------>"""+G+"""
+                   Termux GUI Installer"""+Y+"""
+<------------------------------------------------------------>"""+R+"""
+                  == """+CY+"""By Deadpool2000 """+R+"""==
     
 """)
     try:
@@ -24,38 +24,38 @@ try:
         print(Y+"\n==>"+CY+" Installing XFCE Environment..........."+W)
         os.system("apt install xfce xfce4-terminal tigervnc -y")
         print(R+"""\n
-----------------------------------------------"""+Y+"""
-Installation complete!\n"""+CY+"""
+----------------------------------------------\n"""+R+"""
+== """+Y+"""Installation complete! """+R+"""=="""+CY+"""\n
 Now follow these steps to run Termux-GUI -->
 
-1) Run 'vncserver' command
-2) Add new password for vnc
-3) You will get IP Address like this -
+"""+R+"""1)"""+Y+""" Run """+G+"""'vncserver'"""+Y+""" command
+"""+R+"""2)"""+Y+""" Add new password for vnc
+"""+R+"""3)"""+Y+""" You will get IP Address like this -
 
    localhost:<session_number>
    e.g. localhost:1
-3) After above step,type following command -
+"""+R+"""3)"""+Y+""" After above step,type following command -
 
     DISPLAY=:1 startxfce4 &
 
 Here,1 is a session number.
-4) Install VNC Viewer on your phone
-5) Add IP Address which you got at Step 3
+"""+R+"""4)"""+Y+""" Install VNC Viewer on your phone
+"""+R+"""5)"""+Y+""" Add IP Address which you got at Step 3
     e.g. localhost:1
-6) Add name
-7) Click on Connect
-8) Enter VNC password which you used at Step 2
+"""+R+"""6)"""+Y+""" Add name
+"""+R+"""7)"""+Y+""" Click on Connect
+"""+R+"""8)"""+Y+""" Enter VNC password which you used at Step 2
 
 """+G+"""
 ** Important Note - If you want to exit from GUI,type following command before exit -
                     -> vncserver -kill :<session_number>
                     e.g. vncserver -kill :1
-""")
+"""+W)
     except:
         print(Y+"""
 --------------------------------------------------------------"""+R+"""
 Failed to connect to internet! Check your internet connection :( """+Y+"""
---------------------------------------------------------------""")
+--------------------------------------------------------------\n"""+W)
 except KeyboardInterrupt:
     print("Interrupted ! Good Bye! :) ")
 
